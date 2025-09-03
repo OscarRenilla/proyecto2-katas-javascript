f32b1a98e5519e9ab3172eeb9978954501d354d5
Óscar Reni <oscar06renilla@gmail.com>
2025-09-03 14:18:26 +0200

    Proyecto2

---
+++ ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,10 @@
+let miSuperheroeFavorito = "Hulk";
+let numeroFavorito  = 50;
+let altura  = 5;
+let longitud  = 10;
+let suma  = altura + longitud;
+console.log("Superhéroe favorito:", miSuperheroeFavorito);
+console.log("Número favorito:", numeroFavorito);
+console.log("Altura:", altura);
+console.log("Longitud:", longitud);
+console.log("Suma:", suma); 

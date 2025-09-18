cf8e380b5ce5dc24efbdc575942633239fb60dc4
Óscar Reni <oscar06renilla@gmail.com>
2025-09-18 16:54:44 +0200

    ejercicio 11

--- ejercicios-basicos-avanzados/ejercicio-11.js
+++ ejercicios-basicos-avanzados/ejercicio-11.js
@@ -14,5 +14,16 @@ const mixedElements = [
   "Hola mundo",
 ];
 function averageWord(list) {
-  // Completar
+  let counter = 0;
+  for (const element of list) {
+    if (typeof element === "string") {
+      counter += element.length; 
+    } else {
+      counter += element; 
+    }
+  }
+  const average = counter / list.length; 
+  console.log(average);
 }
+
+averageWord(mixedElements);

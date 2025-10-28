3bea5c15f2f74f2e3c47b462ad125b8711cbaa1b
Óscar Reni <oscar06renilla@gmail.com>
2025-10-28 13:11:20 +0100

    dom - ejercicio-01 (Sin acabar)

---
+++ ejercicios-dom/ejercicio-01/script.js
@@ -0,0 +1,24 @@
+//1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+const showme = document.querySelector(".showme");
+console.log(showme);
+
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+const pillado = document.querySelector("#pillado"); 
+console.log(pillado);
+
+// 1.3 Usa querySelector para mostrar por consola todos los p
+const allP = document.querySelectorAll("p")
+console.log(allP);
+
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+const pokemon = document.querySelectorAll(".pokemon");
+console.log(pokemon);
+
+// 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe". 
+const testMe = document.querySelectorAll
+
+// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo
+
+
+// data-function="testMe".
+
